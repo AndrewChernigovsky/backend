7443dd3747ffa3ecbfbd7b85e35d4ef56d0964f7
Andrew Chernigovsky <chernigovsky108@gmail.com>
2024-01-02 22:33:20 +0300

    2

--- source/data/process.php
+++ source/data/process.php
@@ -5,21 +5,32 @@ if (isset($_POST['submit'])) {
 	$stmt->execute();
 	$post = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+	$oldtitles = $post['title'];
 	$title_id = $post['id'];
 	$title = $_POST['title'];
+
 	$newtitles = $_POST['newtitle'];
 	$remove = $_POST['remove'];
 
-	if (is_array($newtitles)) {
+	echo $oldtitles;
 
-		for ($i = 0; $i < count($newtitles); $i++) {
-			if (isset($newtitles[$i])) {
-				$title_id = $post[$i]['id'];
-				$stmt = $conn->prepare("UPDATE titles SET title = :title WHERE id = :title_id");
-				$stmt->bindParam(':title', $newtitles[$i], PDO::PARAM_INT);
-				$stmt->bindParam(':title_id', $title_id, PDO::PARAM_INT);
-				$stmt->execute();
-				echo $title_id;
+	if (is_array($newtitles)) {
+		$totalCount = count($newtitles);
+
+		for ($i = 0; $i < $totalCount; $i++) {
+			if (isset($newtitles[$i]) && isset($oldtitles[$i])) {
+				// Сравниваем новые и старые значения
+				echo $oldtitles[$i];
+				if ($newtitles[$i] !== $oldtitles[$i]) {
+					$title_id = $post[$i]['id'];
+					// $stmt = $conn->prepare("UPDATE titles SET title = :title WHERE id = :title_id");
+					// $stmt->bindParam(':title', $newtitles[$i], PDO::PARAM_STR);
+					// $stmt->bindParam(':title_id', $title_id, PDO::PARAM_INT);
+
+					// if (!$stmt->execute()) {
+					// 	echo "Ошибка при обновлении записи с ID $title_id";
+					// }
+				}
 			}
 		}
 	}
@@ -28,11 +39,12 @@ if (isset($_POST['submit'])) {
 
 		for ($i = 0; $i < count($newtitles); $i++) {
 
+			echo $remove[$i];
 			if (isset($newtitles[$i]) && $remove[$i] == 'on') {
 				$title_id = $post[$i]['id'];
-				$stmt = $conn->prepare("DELETE FROM titles WHERE id=:title_id");
-				$stmt->bindParam(':title_id', $title_id, PDO::PARAM_INT);
-				$stmt->execute();
+				// $stmt = $conn->prepare("DELETE FROM titles WHERE id=:title_id");
+				// $stmt->bindParam(':title_id', $title_id, PDO::PARAM_INT);
+				// $stmt->execute();
 			}
 
 		}
